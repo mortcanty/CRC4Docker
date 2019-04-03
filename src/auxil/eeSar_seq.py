e473ffaa5c138a24885e00fc0a538640b1fe247e
mortcanty <mort.canty@gmail.com>
2019-04-03 12:07:57 +0200

    improved eesar_seq interface (downloads, metadata)

--- src/auxil/eeSar_seq.py
+++ src/auxil/eeSar_seq.py
@@ -53,6 +53,10 @@ def clipList(current,prev):
     imlist = imlist.add(ee.Image(current).clip(poly))
     return ee.Dictionary({'imlist':imlist,'poly':poly})
 
+def makefeature(data):
+    ''' for exporting as CSV to Drive '''
+    return ee.Feature(None, {'data': data})
+
 def handle_draw(self, action, geo_json):
     global poly
     if action == 'created':
@@ -98,20 +102,25 @@ w_exportname = widgets.Text(
     disabled=False
 )
 w_startdate = widgets.Text(
-    value='2017-04-01',
+    value='2018-04-01',
     placeholder=' ',
     description='Start date:',
     disabled=False
 )
 w_enddate = widgets.Text(
-    value='2017-10-01',
+    value='2018-10-01',
     placeholder=' ',
     description='End date:',
     disabled=False
 )
 w_median = widgets.Checkbox(
-    value=True,
+    value=False,
     description='3x3 Median filter',
+    disabled=False
+)
+w_Q = widgets.Checkbox(
+    value=True,
+    description='Use -2lnQ',
     disabled=False
 )
 w_significance = widgets.BoundedFloatText(
@@ -130,8 +139,8 @@ w_opacity = widgets.BoundedFloatText(
     description='Opacity:',
     disabled=False
 )
-
 w_text = widgets.Textarea(
+    layout = widgets.Layout(width='100%'),
     value = 'Algorithm output',
     rows = 4,
     disabled = False
@@ -143,7 +152,7 @@ w_export = widgets.Button(description='Export to assets',disabled=True)
 w_dates = widgets.HBox([w_relativeorbitnumber,w_startdate,w_enddate])
 w_orbit = widgets.HBox([w_orbitpass,w_platform,w_changemap,w_opacity])
 w_exp = widgets.HBox([w_export,w_exportname])
-w_signif = widgets.HBox([w_significance,w_median])
+w_signif = widgets.HBox([w_significance,w_median,w_Q])
 w_rse = widgets.HBox([w_run,w_preview,w_exp])
 
 box = widgets.VBox([w_text,w_dates,w_orbit,w_signif,w_rse])
@@ -158,17 +167,22 @@ w_relativeorbitnumber.observe(on_widget_change,names='value')
 w_startdate.observe(on_widget_change,names='value')
 w_enddate.observe(on_widget_change,names='value')
 w_median.observe(on_widget_change,names='value')
+w_Q.observe(on_widget_change,names='value')
 w_significance.observe(on_widget_change,names='value')
 
 def on_run_button_clicked(b):
     global result,m,collection,count,timestamplist1, \
            w_startdate,w_enddate,w_orbitpass,w_changemap, \
-           w_relativeorbitnumber,w_significance,w_median, \
-           collectionmean
+           w_relativeorbitnumber,w_significance,w_median,w_Q, \
+           mean_incidence,collectionmean,coords
     try:
-        w_text.value = 'running...'
+        w_text.value = 'running...'       
+        coords = ee.List(poly.bounds().coordinates().get(0))
         collection = ee.ImageCollection('COPERNICUS/S1_GRD') \
-                  .filterBounds(poly) \
+                  .filterBounds(ee.Geometry.Point(coords.get(0))) \
+                  .filterBounds(ee.Geometry.Point(coords.get(1))) \
+                  .filterBounds(ee.Geometry.Point(coords.get(2))) \
+                  .filterBounds(ee.Geometry.Point(coords.get(3))) \
                   .filterDate(ee.Date(w_startdate.value), ee.Date(w_enddate.value)) \
                   .filter(ee.Filter.eq('transmitterReceiverPolarisation', ['VV','VH'])) \
                   .filter(ee.Filter.eq('resolution_meters', 10)) \
@@ -199,7 +213,8 @@ def on_run_button_clicked(b):
         txt += 'Acquisition dates: '+timestamplist[0]+'...'+timestamplist[-1]+'\n'
         txt += 'Relative orbit numbers: '+str(rons)+'\n'
         if len(rons)==1:
-            txt += 'Mean incidence angle: %f'%get_incidence_angle(collection.first())
+            mean_incidence = get_incidence_angle(collection.first())
+            txt += 'Mean incidence angle: %f'%mean_incidence
         else:
             txt += 'Mean incidence angle: (select a rel. orbit)'
         w_text.value = txt
@@ -207,7 +222,7 @@ def on_run_button_clicked(b):
         pList = pcollection.toList(100)   
         first = ee.Dictionary({'imlist':ee.List([]),'poly':poly}) 
         imList = ee.Dictionary(pList.iterate(clipList,first)).get('imlist')
-        result = ee.Dictionary(omnibus(imList,w_significance.value,w_median.value))
+        result = ee.Dictionary(omnibus(imList,w_significance.value,w_median.value,w_Q.value))
         w_preview.disabled = False
 #      display mean of the full collection                
         collectionmean = collection.mean() \
@@ -215,7 +230,7 @@ def on_run_button_clicked(b):
                                .clip(poly) 
         if len(m.layers)>1:
             m.remove_layer(m.layers[1])
-        m.add_layer(TileLayer(url=GetTileLayerUrl( collectionmean.visualize(min=-25, max=10,opacity = 1))))
+        m.add_layer(TileLayer(url=GetTileLayerUrl( collectionmean.visualize(min=-20, max=5,opacity = 1))))
     except Exception as e:
         w_text.value =  'Error: %s'%e
 
@@ -228,6 +243,11 @@ def on_preview_button_clicked(b):
     cmap = ee.Image(result.get('cmap')).byte()
     fmap = ee.Image(result.get('fmap')).byte() 
     bmap = ee.Image(result.get('bmap')).byte() 
+    
+    cmaps = ee.Image.cat(cmap,smap,fmap,bmap).rename(['cmap','smap','fmap']+timestamplist1[1:])
+    downloadpath = cmaps.getDownloadUrl({'scale':10})
+    w_text.value = 'Download change maps from this URL:\n'+downloadpath+'\nNote: This may be unreliable'
+    
     opacity = w_opacity.value
     if w_changemap.value=='First':
         mp = smap 
@@ -248,7 +268,10 @@ w_preview.on_click(on_preview_button_clicked)
 def on_export_button_clicked(b):
     global w_exportname
     collection1 = ee.ImageCollection('COPERNICUS/S2') \
-                    .filterBounds(poly) \
+                    .filterBounds(ee.Geometry.Point(coords.get(0))) \
+                    .filterBounds(ee.Geometry.Point(coords.get(1))) \
+                    .filterBounds(ee.Geometry.Point(coords.get(2))) \
+                    .filterBounds(ee.Geometry.Point(coords.get(3))) \
                     .filterDate(ee.Date(w_startdate.value),ee.Date(w_enddate.value)) \
                     .sort('CLOUDY_PIXEL_PERCENTAGE',True) \
                     .filterMetadata('CLOUDY_PIXEL_PERCENTAGE','less_than',1.0) 
@@ -267,10 +290,32 @@ def on_export_button_clicked(b):
                                 description='assetExportTask', 
                                 assetId=w_exportname.value,scale=10,maxPixels=1e9)
     assexportid = str(assexport.id)
-    w_text.value= 'Exporting to %s\n task id: %s'%(w_exportname.value,assexportid)
+    w_text.value= 'Exporting change maps to %s\n task id: %s'%(w_exportname.value,assexportid)
     assexport.start()  
+#  export metadata to drive
+    times = [timestamp[1:9] for timestamp in timestamplist1]
+    metadata = ee.List(['SEQUENTIAL OMNIBUS: '+time.asctime(),  
+                        'Asset export name: '+w_exportname.value,   
+                        'Orbit pass: '+w_orbitpass.value,    
+                        'Significance: '+str(w_significance.value),  
+                        'Series length: '+str(len(times)),
+                        'Timestamps: '+str(times)[1:-1],
+                        'Rel orbit numbers: '+str(w_relativeorbitnumber.value),
+                        'Platform: '+w_platform.value,
+                        'Mean incidence angles: '+str(mean_incidence),
+                        'Used 3x3 median filter: '+str(w_median.value),
+                        'Used -2lnQ: '+str(w_Q.value)]) \
+                        .cat(['Polygon:']) \
+                        .cat(poly.getInfo()['coordinates'][0])            
+    fileNamePrefix=w_exportname.value.replace('/','-')  
+    gdexport = ee.batch.Export.table.toDrive(ee.FeatureCollection(metadata.map(makefeature)),
+                         description='driveExportTask_meta', 
+                         folder = 'EarthEngineImages',
+                         fileNamePrefix=fileNamePrefix )
+    w_text.value += '\n Exporting metadata to Drive/EarthEngineImages/%s\n task id: %s'%(fileNamePrefix,str(gdexport.id))            
+    gdexport.start()                         
     
-w_export.on_click(on_export_button_clicked)  
+w_export.on_click(on_export_button_clicked) 
 
 def run():
     global m,dc,center
